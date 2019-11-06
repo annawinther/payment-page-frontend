58eb05988ec795bbfd46f9ca5b50782ee9941844
Anna W <anski.anna@gmail.com>
2019-11-06 17:34:27 +0000

    added components and navbar

---
+++ paymentpage/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+function Navbar () {
+    return (
+        <div>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Dash</a>
+                <a href="#">Login</a>
+                <a href="#">Signup</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
