ba01f2acd3bf38d022f6f483eb01432a3f3adad3
Anna Winther <annawinther@annas-mbp.lan>
2019-11-09 11:35:16 +0000

    navbar in progress

--- paymentpage/src/App.js
+++ paymentpage/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-// import Login from './components/Login';
-// import Signup from './components/Signup';
-// import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
 import './App.css';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Navbar></Navbar> */}
-        {/* <Login></Login> */}
-        {/* <Signup></Signup> */}
-        <Counter></Counter>
+        <Navbar></Navbar>
+        <Login></Login>
+        <Signup></Signup>
+        {/* <Counter></Counter> */}
       </header>
     </div>
   );
